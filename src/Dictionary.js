d4d64aaabe9af053312f9402f685a53c08460765
JessCardenas <jesscardenasbcn@gmail.com>
2025-01-10 13:34:41 +0100

    integrating API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    let apiKey = `a1b23b3d4098a0a16ftb2b02cf94o4d8`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={sunset}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
