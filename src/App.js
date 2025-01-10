291b23a8f66535e6578c742f80abd179cec72f33
JessCardenas <jesscardenasbcn@gmail.com>
2025-01-10 12:37:28 +0100

    search engine build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Welcome</header>
+      <div className="container">
+        {" "}
+        <header className="App-header">Welcome</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Jessica Cárdenas</footer>
+      </div>
     </div>
   );
 }
